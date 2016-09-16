370b91b973f1286b30ead4b0e6e23d2b98dbef84
Mateus Gabi <mateusgabimoreira@hotmail.com>
2016-09-15 22:13:30 -0400

    Estrutura do Disco completa

--- src/models/Disco.java
+++ src/models/Disco.java
@@ -8,17 +8,27 @@ package models;
  */
 public class Disco {
 
-	private final Bloco[] array;
+	private final Bloco[] vetor;
 	private final int tamanhoMaximoEmBytes;
 	
 	
 	public Disco(int numeroDeBlocos, int tamanhoMaximoEmBytes) {
 		
-		this.array = new Bloco[numeroDeBlocos];
+		/* d = numeroDeBlocos */
+		
+		this.vetor = new Bloco[numeroDeBlocos];
 		this.tamanhoMaximoEmBytes = tamanhoMaximoEmBytes;
 		
 		/* O primeiro bloco é o bloco de diretório */
-		array[0] = new BlocoDiretorio(numeroDeBlocos - 3, tamanhoMaximoEmBytes);
+		vetor[0] = new BlocoDiretorio(numeroDeBlocos - 3, tamanhoMaximoEmBytes);
+		
+		/* O segundo bloco é o bloco com uma lista de Blocos Livres*/
+		vetor[1] = new BlocoLivre();
+		
+		/* Bloco de indices */
+		vetor[2] = new BlocoIndice();
+		
+		/* do bloco 3 ao d - 1 são blocos de dados */
 		
 	}
 	
