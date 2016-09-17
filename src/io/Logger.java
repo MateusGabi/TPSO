6ad29455d9b8532b25ebffc3d66158154c955915
Mateus Gabi <mateusgabimoreira@hotmail.com>
2016-09-17 15:01:48 -0400

    Adicionado Interface para registro de eventos do sistema em io/Logger

---
+++ src/io/Logger.java
@@ -0,0 +1,21 @@
+package io;
+
+/**
+ * Um Logger serve para escrever em um arquivo e no terminal os eventos do
+ * sistema (Especificação y).
+ * 
+ * @author Mateus Gabi
+ *
+ */
+public class Logger {
+
+	public static void log(String mensagem) {
+		
+		/* Escrever no arquivo log.txt */
+		
+		/* Imprime mensagem no terminal */
+		System.out.println(mensagem);
+		
+	}
+
+}
